f3d67613442574109272fa0a040d0bc821975025
Star24680 <gulerumaan1@gmail.com>
2024-08-13 18:12:30 +0500

    Initial commit

---
+++ TypeCasting.py
@@ -0,0 +1,19 @@
+Name="Gul"
+Age=12
+Weight=43.5
+Student=True
+print("Name" ,Name)
+print("type of name", type(Name))
+print("Age" ,Age)
+print("Type of Age", type(Age))
+print("Weight" ,Weight)
+print("Type of Weight", type(Weight))
+print("Student" , Student)
+print("Type of student", type(Student))
+print("After Typecasting")
+Age=float(Age)
+print("Age" ,Age)
+print("Type of Age", type(Age))
+Weight=int(Weight)
+print("Weight" ,Weight)
+print("Type of Weight", type(Weight))
